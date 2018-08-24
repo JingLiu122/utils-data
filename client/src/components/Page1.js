c6fba5aa4bbebf16e975500bc4f941e3bfd539fb
Steve Mu <skycloud112@gmail.com>
2018-08-23 22:13:34 -0400

    add

--- client/src/components/Page1.js
+++ client/src/components/Page1.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { LineChart, Line, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+
+
+let data = [{ name: 'a', value: 12 }]
 
 class Page1 extends Component {
 
@@ -10,20 +14,28 @@ class Page1 extends Component {
     let res = await fetch("http://localhost:3000/data");
     let data = await res.json();
     // console.log(data);
-    this.setState({data});
+    this.setState({ data });
 
   }
 
 
   render() {
     return (
-        <div>
-          page1
-          {JSON.stringify(this.state.data)}
-          <button onClick={() => {
-            this.props.history.push("/page2")
-          }}>go to page2</button>
-        </div>
+      <div>
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+        {JSON.stringify(this.state.data)}
+        <button onClick={() => {
+          this.props.history.push("/page2")
+        }}>go to page2</button>
+      </div>
     );
   }
 }
